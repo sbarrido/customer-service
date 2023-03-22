4d24933f635bcf54ea130b510facfdcad1f54d0f
Sam Barrido <ssbarrido@gmail.com>
2023-03-22 17:33:47 -0400

    customer controller and model creation

---
+++ src/main/java/edu/iu/c322/customerservice/controller/CustomerController.java
@@ -0,0 +1,10 @@
+package edu.iu.c322.customerservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+}
